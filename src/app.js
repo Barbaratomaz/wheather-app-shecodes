c4cdc96eec64f973705f1a4d005ce97b0b989832
Barbaratomaz <84580943+Barbaratomaz@users.noreply.github.com>
2022-03-01 09:09:59 +0000

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ function formatDate(timestamp) {
 
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    minures = `0${minutes}`;
+    minutes = `0${minutes}`;
   }
 
   let days = [
@@ -55,8 +55,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "9b06bfd491f116ce539dc3e30afcca26";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "9b06bfd491f116ce539dc3e30afcca26";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
